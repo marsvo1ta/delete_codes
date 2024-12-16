245eed2c70aa66205ac97194ce4ecbcc882e78e4
marsvo1ta <euromobilco@gmail.com>
2024-12-16 14:12:27 +0200

    delete hs

--- delete_codes.py
+++ delete_codes.py
@@ -1,5 +1,8 @@
 from openpyxl import load_workbook
+from api_request import ApiRequest
 
+
+api = ApiRequest()
 
 def process_excel_column_a(file):
     workbook = load_workbook(file)
@@ -7,9 +10,9 @@ def process_excel_column_a(file):
 
     result = []
     for row in sheet.iter_rows(min_col=1, max_col=1, values_only=True):
-        cell_value = row[0]
+        cell_value: str = row[0]
         result.append(cell_value)
-
+    api.bulk_delete(result)
     workbook.close()
     return result
 
